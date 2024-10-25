35b2ac8b655c1fd9a0d0df92ee166717c510c29e
Alex Arteaga <ajarteag@andrew.cmu.edu>
2024-10-24 18:30:02 -0700

    added tty true flag to keep container running

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ source /opt/ros/noetic/setup.bash
 # Build catkin_ws
 cd barracuda-imu/catkin_ws
 catkin_make
+source devel/setup.bash
 
 # Start interactive shell session in /opt/barracuda-imu directory
 cd ..
