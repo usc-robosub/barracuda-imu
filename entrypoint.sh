bee7da19ff1ba397abc13bec5772d739379d2e76
Alex Arteaga <ajarteag@andrew.cmu.edu>
2024-10-28 19:02:27 -0700

    created udev rule template

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/bash
 source /opt/ros/noetic/setup.bash
 
-# Build catkin_ws
+# Build & Source catkin_ws
 cd barracuda-imu/catkin_ws
 catkin_make
 source devel/setup.bash
+
+# Create udev rules file for NGIMU (need to update the IDVendor and IDProduct values)
+echo 'SUBSYSTEM=="tty", ATTR{idVendor}=="1234", ATTR{idProduct}=="5678", SYMLINK+="ngimu"' > /etc/udev/rules.d/99-ngimu.rules
+udevd &
 
 # Start interactive shell session in /opt/barracuda-imu directory
 cd ..
