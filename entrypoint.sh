9b9a32e4ee12a59d59cf7a5581eb193eafd49a71
Alex Arteaga <ajarteag@andrew.cmu.edu>
2024-11-04 18:23:45 -0800

    changed vendor and product id to use env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ catkin_make
 source devel/setup.bash
 
 # Create udev rules file for NGIMU (need to update the IDVendor and IDProduct values)
-echo 'SUBSYSTEM=="tty", ATTR{idVendor}=="1234", ATTR{idProduct}=="5678", SYMLINK+="ngimu"' > /etc/udev/rules.d/99-ngimu.rules
+echo "SUBSYSTEM==\"tty\", ATTR{idVendor}==\"$ID_VENDOR\", ATTR{idProduct}==\"$ID_PRODUCT\", SYMLINK+=\"ngimu\"" > /etc/udev/rules.d/99-ngimu.rules
 udevd &
 
 # Start interactive shell session in /opt/barracuda-imu directory
