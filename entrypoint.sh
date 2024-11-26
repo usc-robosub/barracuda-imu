43704f4f05363f6ddf48ccdd381a4affa4acc545
Alex Arteaga <ajarteag@andrew.cmu.edu>
2024-11-25 21:44:00 -0800

    fix: update udev rules for NGIMU to remove unnecessary action specification

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ catkin_make
 source devel/setup.bash
 
 # Create udev rules file for NGIMU
-echo 'ACTION=="add", SUBSYSTEMS=="tty", ATTRS{idVendor}=="${VENDOR_ID}", ATTRS{idProduct}=="${PRODUCT_ID}", SYMLINK+="ngimu"' > /etc/udev/rules.d/99-ngimu.rules
+echo 'SUBSYSTEM=="tty", ATTRS{idVendor}=="${VENDOR_ID}", ATTRS{idProduct}=="${PRODUCT_ID}", SYMLINK+="ngimu"' > /etc/udev/rules.d/99-ngimu.rules
 sudo udevadm control --reload-rules 
 sudo service udev restart 
 sudo udevadm trigger
